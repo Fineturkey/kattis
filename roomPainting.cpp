733c5da4e4a67422b665a4c330212c1f4febc27e
Eugene <eugene7098@gmail.com>
2026-07-18 13:32:05 -0600

    roompainting Complete

--- roomPainting.cpp
+++ roomPainting.cpp
@@ -4,32 +4,40 @@
 
 using namespace std;
 
-int main() {
-    int m, n, totalWaste;
-    cin >> m >> n;
+int main()
+{
+    int n, m;
+    int totalWaste = 0;
+
+    cin >> n >> m;
 
     vector<int> numShop(n);
     vector<int> numNeed(m);
 
-    for(int i = 0; i < n; i++) { cin >> numShop[i]; }
-    for(int i = 0; i < m; i++) { cin >> numNeed[i]; }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> numShop[i];
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> numNeed[i];
+    }
 
     sort(numShop.begin(), numShop.end());
-    sort(numNeed.begin(), numNeed.end());
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(numNeed[i] > numShop[j]) {
-                j++;
-            }
-            else{
-                totalWaste += numNeed[i] - numShop[j--];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (numNeed[i] <= numShop[j])
+            {
+                totalWaste += numShop[j] - numNeed[i];
                 break;
             }
         }
     }
-
-    cout << totalWaste;
+    cout << totalWaste << endl;
 
     return 0;
 }
