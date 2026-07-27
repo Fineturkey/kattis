a712c2b04b4405299cf9309a0d68f9d90afb448f
Eugene Gu <eugene7098@gmail.com>
2026-07-27 12:59:05 -0600

    Muppet

--- 2026/chineseremainder.cpp
+++ 2026/chineseremainder.cpp
@@ -24,13 +24,25 @@ int main() {
 
     for(int i = 0; i < numCase; i++) {
         int a, n, b, m;
-        int x, k = 0;
+        int k = 0;
+        int x = 0;
+        int y = 1;
         cin >> a >> n >> b >> m;
 
         k = lcm(n, m);
 
         if(check(a, n, b, m)) {
+            while(x != y) {
+                x = a % n;
+                y = b % m;
 
+                if(x == y) {
+                    break;
+                }
+                a += n;
+                b += m;
+            }
+            cout << k << x << endl;
         }
         else {
             cout << "no solution";
